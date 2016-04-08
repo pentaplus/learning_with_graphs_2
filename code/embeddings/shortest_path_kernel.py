corrected runtime computation for eigen kernel

code/embeddings/shortest_path_kernel.py
@@ -21,6 +21,7 @@ import sys
 import time
 
 from os.path import abspath, dirname, join
+from scipy.sparse import csr_matrix, lil_matrix
 
 
 # determine script path
@@ -30,7 +31,7 @@ SCRIPT_FOLDER_PATH = dirname(abspath(SCRIPT_PATH))
 # of the script's parent directory
 sys.path.append(join(SCRIPT_FOLDER_PATH, '..'))
 
-from misc import pz
+from misc import floyd_warshall, pz
 
 
 
@@ -48,9 +49,12 @@ def compute_kernel_mat(graph_meta_data_of_num, param_range = [None]):
     
 
     #=============================================================================
-    # 1) precompute the (sparse) adjacency matrices of the graphs in the dataset
+    # compute kernel matrix over all graphs in the dataset
     #=============================================================================
-    adj_mats = []
+    
+    Ds = []
+        
+    max_path = 0
     
 #    for i in xrange(num_graphs):
     for i, (graph_path, class_lbl) in \
@@ -63,39 +67,42 @@ def compute_kernel_mat(graph_meta_data_of_num, param_range = [None]):
         # load graph
         G = pz.load(graph_path)
         # determine its adjacency matrix
-        A = nx.adj_matrix(G, weight = None)
+        A = nx.adj_matrix(G, weight = None).astype('d').toarray()
         
-        adj_mats.append(A)
+        is_symmetric = not nx.is_directed(G)
         
-    
-    #=============================================================================
-    # 2) compute kernel matrix over all graphs in the dataset
-    #=============================================================================
-    for i in xrange(num_graphs):
-        A_i = adj_mats[i].todense()
+        sys.modules['__main__'].A = A
+        
+#        adj_mats.append(A)
+        
+        D = floyd_warshall.floyd_warshall(A, is_symmetric)
+        Ds.append(D)
+        
+        sys.modules['__main__'].Ds = Ds
 
-        for j in xrange(i, num_graphs):
-            A_j = adj_mats[j].todense()
-            
-            # !!
-#            sys.modules['__main__'].A_j = A_j
-            
-            # apply preconditioned conjugate gradient method
-            b = np.ones((A_i.shape[0] * A_j.shape[0], 1))
-            
-                
-            
-            kernel_mat[i,j] = np.sum(x)
-            if i != j:
-                kernel_mat[j,i] = kernel_mat[i,j]
-            
+        aux = D[np.isfinite(D)].max()
+        
+        if aux > max_path:
+            max_path = aux
 #             # !!
 ##            sys.modules['__main__'].kernel_mat = kernel_mat
             
 #            print 'i =', i, 'j =', j
-            print 'i =', i, 'j =', j, kernel_mat[i,j]
-
-    
+#            print 'i =', i, 'j =', j, kernel_mat[i,j]
+    sp = lil_matrix((max_path + 1), num_graphs)
+    for i in xrange(num_graphs):
+        D = Ds[i]
+        sys.modules['__main__'].D = D
+        
+        I = np.triu(np.isfinite(D))
+        
+        Ind = D[I].astype(np.int64)
+        
+        aux = np.bincount(Ind)
+        
+        sp[Ind, i] = aux[Ind]
+        
+    kernel_mat = sp.T * sp
 
     kernel_mat_of_param[None] = kernel_mat
     

code/misc/floyd_warshall.py
@@ -31,14 +31,14 @@ SCRIPT_FOLDER_PATH = dirname(abspath(SCRIPT_PATH))
 sys.path.append(join(SCRIPT_FOLDER_PATH, '..'))
 
 
-def floyd_warshall(A):
+def floyd_warshall(A, sym):
 
     
 #    n = size(A,1); % number of nodes
     n = A.shape[0]
     
 #    D=zeros(n,n);
-    D = np.zeros(n, n)
+    D = np.zeros((n, n))
     
     
 #    if nargin<3 % if the graph is not weighted, then
@@ -52,10 +52,11 @@ def floyd_warshall(A):
 #    D(A+diag(repmat(Inf,n,1))==0)=Inf; % If A(i,j)~=0 and i~=j D(i,j)=Inf;
     # !!
     # repmat(Inf, n, 1)
-    D[A + np.diag(np.tile(np.inf, n)) == 0] = np.inf
+    D[A + np.diag(np.tile(np.inf, n)) == 0.] = np.inf
         
     
-    D=full(D.*(ones(n)-eye(n))); % set the diagonal to zero
+#    D=full(D.*(ones(n)-eye(n))); % set the diagonal to zero
+    np.fill_diagonal(D, 0)
     
 #    %t=cputime;
 #    if sym % then it is a bit faster
@@ -65,9 +66,33 @@ def floyd_warshall(A):
 #        D(Sumdist<D)=Sumdist(Sumdist<D);
 #      end
 #    else  
-      for k=1:n
-        Daux1=repmat(full(D(:,k)),1,n);
-        Daux2=repmat(full(D(k,:)),n,1);
-        Sumdist=Daux1+Daux2;
-        D(Sumdist<D)=Sumdist(Sumdist<D);
+    
+    if sym:
+        for k in xrange(n):
+            D_aux = np.tile(D[:, k: k + 1], (1, n))
+            Sum_dist = D_aux + D_aux.T
+            
+#            sys.modules['__main__'].D_aux = D_aux
+#            sys.modules['__main__'].D = D
+#            sys.modules['__main__'].Sum_dist = Sum_dist 
+            
+            D[Sum_dist < D] = Sum_dist[Sum_dist < D]
+    else:
+        for k in xrange(n):
+            D_aux_1 = np.tile(D[:, k: k + 1], (1, n))
+            D_aux_2 = np.tile(D[k, :], (n, 1))
+            Sum_dist = D_aux_1 + D_aux_2
+            D[Sum_dist < D] = Sum_dist[Sum_dist < D]
+            
+    return D
+
+
+if __name__ == '__main__':
+    D = floyd_warshall(A, True)
+    #D2 = floyd_warshall(A, False)
+    
+    
+    
+    #import scipy.io as sio
+    #sio.savemat('D2.mat', {'D': D})
 